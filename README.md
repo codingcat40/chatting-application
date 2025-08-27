62ed8d9179a42812d66a0c5e0a88f38a6e12e3dd
Naman Matoliya <naman@fieldproxy.com>
2025-08-27 12:09:20 +0530

    Login, Signup, Profile Pages

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ set up tailwind + vite enviornment
 
 --> working on right sidebar (profile pic, images, logout buttonn)
 -->  finished the Home page of the application
+--> finished coding Login/Signup Pages
+--> Finished working on Profile page to edit the profile info
+
+--> With this I finished working on Front end of the QuickChat
+
+
+
+
+<!-- Start working on Backend Setup -->
+
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import assets from '../assets/assets'
 const LoginPage = () => {
+  const [currState, setCurrState] = useState("Sign Up");
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [bio, setBio] = useState("");
+  const [isDataSubmitted, setIsDataSubmitted] = useState(false);
+
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if(currState === "Sign Up" && !isDataSubmitted){
+      setIsDataSubmitted(true);
+      return;
+    }
+  }
+
+
   return (
-    <div>
-        <h1>Login Page</h1>
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+        {/* left */}
+        <img src={assets.logo_big} alt="" className='w-[min(30vw, 250px)]'/>
+        {/* right */}
+        <form onSubmit={onSubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+            <h2 className='font-medium text-2xl flex justify-between items-center'>{currState}
+            {
+              isDataSubmitted && 
+              <img onClick={() => setIsDataSubmitted(false)} src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>
+            }
+
+            </h2>
+            {currState === "Sign Up" && !isDataSubmitted && (<input type="text" value={fullName} onChange={(e) =>setFullName(e.target.value)} className='p-2 border border-gray-500 rounded-md focus:outline-none' placeholder='Full Name' required/>
+)}
+
+        {!isDataSubmitted && (
+          <>
+          <input value={email} type="email" required placeholder='Email Address' className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500' onChange={(e)=>setEmail(e.target.value)}/>
+          <input value={password} type="password" required placeholder='Password' className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500' onChange={(e)=>setPassword(e.target.value)}/>
+          
+          </>
+        )}
+
+        {
+          currState === "Sign Up" && isDataSubmitted && (
+            <textarea onChange={(e) =>setBio(e.target.value)} rows={4} className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500' placeholder='Provide a short bio...' value={bio} required></textarea>
+          )
+        }
+
+        <button type='submit' className='py-3 bg-gradient-to-r from-purple-400 to-voilet-600 text-white rounded-md cursor-pointer'>
+          {
+            currState === "Sign Up" ? "Create Account" : "Login Now"
+          }
+        </button>
+
+        <div className='flex items-center gap-2 text-sm text-gray-500'>
+          <input type="checkbox" />
+          <p>Agree to the terms of use and privacy policy.</p>
+        </div>
+
+
+        <div className='flex flex-col gap-2'>
+            {currState === "Sign Up" ? (
+              <p className='text-sm text-gray-600'>Already have an account? <span onClick={() => {setCurrState("Login"); setIsDataSubmitted(false)}} className='font-medium text-violet-500 cursor-pointer'>Login here</span></p>
+            ) : (<p className='text-sm text-gray-600'>New User? <span onClick={() => {setCurrState("Sign Up") }} className='font-medium text-violet-500 cursor-pointer'>SignUp here</span></p>)}
+        </div>
+        </form>
     </div>
   )
 }
