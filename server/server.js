1343a171f55bcfd6fb72e5a5f364b4f92895e109
Naman Matoliya <naman@fieldproxy.com>
2025-09-12 19:46:09 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -4,11 +4,38 @@ import cors from 'cors'
 import http from 'http'
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
-
+import messageRouter from './routes/messageRoutes.js';
+import { Server } from "socket.io";
 // express server
 
 const app = express();
 const server = http.createServer(app);
+
+// socket io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+// store online users
+export const userSocketMap = {};
+// {userId: socketId}
+
+// socket io connection handler
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User connected", userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    // Emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId]
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+    })
+})
 
 // middlewares
 app.use(express.json({limit: '4mb'}));
@@ -21,7 +48,7 @@ app.use("/api/status", (req, res) => {
 });
 
 app.use("/api/auth", userRouter);
-
+app.use("/api/messages", messageRouter);
 
 
 
